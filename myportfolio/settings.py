692f06b3510c0a1d40591a1dcaad4c2906f58364
karimovsardorbek <karimovsardor206@gmail.com>
2024-05-30 16:56:31 +0500

    added html and some urls

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -122,14 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 import os
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
 
 
 # Default primary key field type
--- portfolio/views.py
+++ portfolio/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 from .models import Profile
 
 def home(request):
-    profile = Profile.objects.all()
-    return render(request, 'home.html', {'projects': profile})
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def projects(request):
+    return render(request, 'projects.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
