3b1b5d8d9e53462c3bc19456e6513d5e02f6ecc8
karimovsardorbek <karimovsardor206@gmail.com>
2024-05-31 15:28:24 +0500

    fixed little detail

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'portfolio',
         'USER': 'root',
-        'PASSWORD': 'sardorbekA7071',
+        'PASSWORD': 'yourpassword',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
